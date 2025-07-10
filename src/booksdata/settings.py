2396181b5732dfe0f82618025aca96b14294855a
doodosina-c <1mail4man@proton.me>
2025-07-10 11:46:07 +0500

    Message

--- src/booksdata/settings.py
+++ src/booksdata/settings.py
@@ -1,8 +1,9 @@
 import yaml
 from yaml.loader import SafeLoader
 from pathlib import Path
+from typing import Iterable, Any
 
-def load_config():
+def load_config() -> Iterable[str | dict[str, Any]]:
     with open(Path(__file__).parent / "config.yaml") as file:
         if isinstance((data :=  yaml.load(file, SafeLoader)), dict):
             return data.values()
